5c08b3bdfdb357e6fac3fab8e0170824e70abb34
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2024-03-20 12:18:07 -0500

    models

---
+++ database/database.go
@@ -0,0 +1,32 @@
+package databaseimport
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func ConnectToDb() *gorm.DB {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env files")
+	}
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to the database")
+	}
+
+	fmt.Println("Connected to the database")
+	return database
+}
