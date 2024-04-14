a00b8c45e770dab9d7b00456949d138400c423d9
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2024-04-14 17:20:04 -0500

    docker config

--- models/user.go
+++ models/user.go
@@ -30,21 +30,21 @@ func GetUser(db *gorm.DB) gin.HandlerFunc {
 		tokenString, _ := c.Request.Cookie("Token")
 		idUser := GetIdJWT(tokenString.Value)
 		var user User
-		db.Preload("MessageForum").Preload("Fighters").Preload("Fighters.History").First(&user, idUser)
+		db.Preload("MessagesForum").Preload("Fighters").First(&user, idUser)
 		c.JSON(200, user)
 	}
 }
 func GetAllUser(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var user []User
-		db.Preload("MessageForum").Preload("Fighters").Find(&user)
+		db.Preload("MessagesForum").Preload("Fighters").Find(&user)
 		c.JSON(200, user)
 	}
 }
 func GetOtherUser(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var user User
-		db.Preload("MessageForum").Preload("Fighters").First(&user, c.Param("id"))
+		db.Preload("MessagesForum").Preload("Fighters").First(&user, c.Param("id"))
 		c.JSON(200, user)
 	}
 }
